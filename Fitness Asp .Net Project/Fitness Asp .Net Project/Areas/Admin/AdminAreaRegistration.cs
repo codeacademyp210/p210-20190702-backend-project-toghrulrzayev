1bff6764ddc41d043495197d1cc70bd18a660903
togrulrzayev123 <ToghrulRr@code.edu.az>
2019-07-03 17:37:16 +0400

    Initialize

---
+++ Fitness Asp .Net Project/Fitness Asp .Net Project/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace Fitness_Asp.Net_Project.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Fitness_Asp.Net_Project.Areas.Admin.Controllers" }
+            );
+            context.MapRoute(
+                "Admin_default_old",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Fitness_Asp.Net_Project.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ Fitness Asp .Net Project/Fitness Asp .Net Project/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Fitness_Asp.Net_Project.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
